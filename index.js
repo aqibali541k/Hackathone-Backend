66cb2befa6e8e83abc510ca294fe65e93dc4e7f2
Aqib Shabbir <aqibali541k@gmail.com>
2026-01-19 19:04:37 +0500

    Crash fix

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.get("/", (req, res) => {
   res.send("server is online");
 });
 
-// app.listen(port, () => {
-//   console.log(`Your server is online at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Your server is online at http://localhost:${port}`);
+});
