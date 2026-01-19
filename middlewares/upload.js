a923f9e719edbaaf81d717bca7452b1a439a02bf
Aqib Shabbir <aqibali541k@gmail.com>
2026-01-19 19:15:52 +0500

    bug fix 3

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,21 +1,8 @@
-import multer from "multer";
-import fs from "fs";
+const multer = require("multer");
 
-const uploadDir = "/tmp/uploads";
-
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 }, // optional
 });
 
-const upload = multer({ storage });
-
-export default upload;
+module.exports = upload;
