9bc5c00a3bd537e5110094a7d8225080ada2f6c6
aqibali541k <aqibali541k@gmail.com>
2026-05-05 10:45:55 -0700

    debug the contact page

--- index.js
+++ index.js
@@ -52,9 +52,10 @@ app.get("/", (req, res) => {
 });
 
 /* ---------------- START SERVER ---------------- */
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+// const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("🚀 Server is online");
 });
 
+/* ---------- EXPORT (NO app.listen) ---------- */
 module.exports = app;
