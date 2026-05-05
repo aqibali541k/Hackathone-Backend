e6224e69eed0d8c6affec8faa3a8c55c91add564
aqibali541k <aqibali541k@gmail.com>
2026-05-05 11:03:34 -0700

    debug the contact page

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use("/analytics", analyticsRouter);
 app.use("/contact", contactRouter);
 
 app.get("/", (req, res) => {
-  res.send("🚀 Donation Hub Server is Online");
+  res.send(" Donation Hub Server is Online");
 });
 
 /* ---------- EXPORT (NO app.listen) ---------- */
